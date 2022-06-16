59a8daabde027e8509da082c2acfff90e9cb9436
jeanfernandesComp1997 <jeanfernandes10@hotmail.com>
2022-06-15 23:35:57 -0300

    Add exception

--- src/main/kotlin/br/com/bytebank/main.kt
+++ src/main/kotlin/br/com/bytebank/main.kt
@@ -1,3 +1,5 @@
+import br.com.bytebank.models.Address
+
 fun main() {
     println("Init main")
     function1()
@@ -14,6 +16,8 @@ fun function2() {
     println("Init function2")
     for (i in 1..5) {
         println(i)
+        val address = Any()
+        address as Address
     }
     println("End function2")
 }
