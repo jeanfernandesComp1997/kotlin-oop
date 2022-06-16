ce8e3d8f7fd8fcc26478ebf332b2b62f3a7679de
jeanfernandesComp1997 <jeanfernandes10@hotmail.com>
2022-06-16 20:15:34 -0300

    Fix exception

--- src/main/kotlin/br/com/bytebank/main.kt
+++ src/main/kotlin/br/com/bytebank/main.kt
@@ -1,19 +1,35 @@
+package br.com.bytebank
+
 import br.com.bytebank.models.Address
 
 fun main() {
     println("Init main")
+
+    try {
+        10 / 0
+    } catch (ex: Exception) {
+        println("Error: $ex")
+    }
+
     function1()
     println("End main")
 }
 
 fun function1() {
     println("Init function1")
-    function2()
+
+    try {
+        function2()
+    } catch (ex: Exception) {
+        println("Error: $ex")
+        println("Instance of: ${ex.javaClass}")
+    }
     println("End function1")
 }
 
 fun function2() {
     println("Init function2")
+
     for (i in 1..5) {
         println(i)
         val address = Any()
