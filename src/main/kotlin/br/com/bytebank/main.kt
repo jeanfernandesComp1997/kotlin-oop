688b06247374c1bda6feef6966d5571b02a73b95
jeanfernandesComp1997 <jeanfernandes10@hotmail.com>
2022-06-16 22:02:58 -0300

    Add elvis operator

--- src/main/kotlin/br/com/bytebank/main.kt
+++ src/main/kotlin/br/com/bytebank/main.kt
@@ -6,7 +6,16 @@ fun main() {
     var address: Address? = null
     println(address?.street?.length)
 
-    address?.let { address: Address ->
-        println(address.street.length)
+    address?.let { addressLambda: Address ->
+        println(addressLambda.street.length)
     }
+
+    println(address?.street?.length ?: 0)
+
+    test(1)
+}
+
+fun test(value: Any) {
+    val number: Int? = value as? Int
+    println(number)
 }
