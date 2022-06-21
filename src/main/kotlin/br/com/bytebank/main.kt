16d226c41e9fb10bf8ef5e782db28a8d605fd067
jeanfernandesComp1997 <jeanfernandes10@hotmail.com>
2022-06-20 22:53:53 -0300

    Add functions with params

--- src/main/kotlin/br/com/bytebank/main.kt
+++ src/main/kotlin/br/com/bytebank/main.kt
@@ -2,6 +2,7 @@ package br.com.bytebank
 
 fun main() {
     testTypeFunctionReference()
+    testClassFunction()
 
     val myLambdaFunction: () -> Unit = {
         println("Execute with lambda")
@@ -15,21 +16,24 @@ fun main() {
 }
 
 fun testTypeFunctionReference() {
-    val myFunction: () -> Unit = ::test
-    println(myFunction())
+    val myFunction: (Int, Int) -> Int = ::sum
+    println(myFunction(5, 10))
+}
 
-    val myFunctionClass: () -> Unit = Test()
-    println(myFunctionClass())
+fun testClassFunction() {
+    val myFunctionClass: (Int, Int) -> Int = Sum()
+    println(myFunctionClass(10, 10))
 }
 
 fun test() {
     println("Execute test")
 }
 
-// fun sum(a: Int, b: Int): Int = a + b
+fun sum(a: Int, b: Int): Int = a + b
 
-class Test : () -> Unit {
-    override fun invoke() {
-        println("Execute invoke")
+class Sum : (Int, Int) -> Int {
+    override fun invoke(a: Int, b: Int): Int {
+        return a + b
     }
+
 }
