56890b0c9c064825d95036e2d70d8e366b8afd62
jeanfernandesComp1997 <jeanfernandes10@hotmail.com>
2022-06-21 21:45:15 -0300

    High order functions

--- src/main/kotlin/br/com/bytebank/models/InternSystem.kt
+++ src/main/kotlin/br/com/bytebank/models/InternSystem.kt
@@ -4,9 +4,10 @@ import br.com.bytebank.models.Authenticable
 
 class InternSystem {
 
-    fun enter(admin: Authenticable, password: Int) {
+    fun enter(admin: Authenticable, password: Int, authenticated: () -> Unit = {}) {
         if (admin.login(password)) {
             println("Welcome to ByteBank")
+            authenticated()
         } else {
             println("Authentication Failed")
         }
